705e563027bf993263c560f88a54102a91362b98
AkiraLiu39 <>
2018-05-02 18:22:06 +0800

    yohoho

--- config-server/src/main/java/com/akira/demo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/akira/demo/configserver/ConfigServerApplication.java
@@ -3,9 +3,12 @@ package com.akira.demo.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+//集群配置，注册eureka服务中心
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
